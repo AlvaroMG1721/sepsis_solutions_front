d2fb30909b937ab7636c5726ff9616a97ef9075c
Álvaro Márquez <alvaro.marquez.gomez@everis.com>
2019-02-06 20:44:45 +0100

    Eliminados métodos innecesarios.

--- app/models/dao/PatientDAO.scala
+++ app/models/dao/PatientDAO.scala
@@ -16,64 +16,6 @@ object PatientDAOImpl extends PatientDAO {
   val emf: EntityManagerFactory = Persistence.createEntityManagerFactory("cassandra_employees")
 
   /**
-   * @param employee
-   * @return
-   */
-  def listAlerts(employee: Employee): Either[String, String] = {
-    try {
-      val em = emf.createEntityManager()
-      em.persist(employee)
-      em.close()
-      Right("Employee has been created successfully.")
-    }
-    catch {
-      case ex: Exception => logger.error("Error : ", ex); Left(ex.getMessage())
-    }
-  }
-
-  def update(employee: Employee): Either[String, String] = {
-    try {
-      val em = emf.createEntityManager()
-      em.merge(employee)
-      em.close()
-      Right("Employee has been updated successfully.")
-    }
-    catch {
-      case ex: Exception => logger.error("Error : ", ex); Left(ex.getMessage())
-    }
-  }
-
-  def delete(id: String): Either[String, String] = {
-    try {
-      val em = emf.createEntityManager()
-      val query = em.createQuery(s"SELECT e FROM Employee e WHERE e.id = '$id'")
-      val employees: List[Employee] = query.getResultList().toArray.toList.asInstanceOf[List[Employee]]
-      em.remove(employees.head)
-      em.close()
-      Right("Employee has been updated successfully.")
-    }
-    catch {
-      case ex: Exception => logger.error("Error : ", ex); Left(ex.getMessage())
-    }
-  }
-
-  /**
-   * @return
-   */
-  def findAll(): Either[String, List[Employee]] = {
-    try {
-      val em = emf.createEntityManager()
-      val query = em.createQuery(s"SELECT * FROM Employee e")
-      val employees: List[Employee] = query.getResultList().toArray.toList.asInstanceOf[List[Employee]]
-      em.close()
-      Right(employees)
-    }
-    catch {
-      case ex: Exception => logger.error("Error : ", ex); Left(ex.getMessage())
-    }
-  }
-
-  /**
    * Return a page of (Patient) with score clasified as alert.
    */
   def listAlerts(page: Int = 0, orderBy: Int = 1, filter: String = "%"): Either[String, Page[Employee]] = {
